4fe794b335215460f6dc6f5df2185fd958c41b21
Maximiliano Garcia <mrozado@lagash.com>
2018-12-14 15:22:21 -0300

    GraphQL Endpoint created

--- graph-with-db/index.js
+++ graph-with-db/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const graphQLHttp = require("express-graphql");
 const app = express();
 const port = 5500;
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get(
+  "/graphql",
+  graphQLHttp({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
